8bdfc945593e80f3737211a4d56e6dcaca6c6f99
EMMYbest1 <emmajerry2016@gmail.com>
2021-03-06 14:06:25 +0200

    Update

--- src/routes/routes.component.tsx
+++ src/routes/routes.component.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {routes} from '../db/routes.json';
 
+console.log(routes);
 export const Routes: React.FC = (): JSX.Element => {
   return (
     <Switch>
-      <Route exact path="/" render={() => <div>Testing</div>} />
+     {routes}
     </Switch>
   );
 };
