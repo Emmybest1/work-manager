b86db8d3c228e9ccc02daeba606a8e0e33f51200
EMMYbest1 <emmajerry2016@gmail.com>
2021-03-07 19:01:26 +0200

    Update

--- src/components/pages/home/home.component.tsx
+++ src/components/pages/home/home.component.tsx
@@ -4,35 +4,41 @@ import {Main} from '../../structures/main/main.component';
 import Button, {ButtonVariation} from '../../partials/button/button.component';
 import Input from '../../partials/input/input.component';
 import './home.style.scss';
+import Footer from '../../structures/footer/footer.component';
 
 const Home: React.FC = (): JSX.Element => {
   const [emailToRegister, setEmailToRegister] = useState<string>('');
   const [addEmailInputId] = useUniqueIds(1);
   return (
-    <Main>
-      <div
-        className="home-container"
-        style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/home-bg.jpg)`}}
-      ></div>
+    <>
+      <Main>
+        <div
+          className="home-container"
+          style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/home-bg.jpg)`}}
+        ></div>
 
-      <div className="home-container__overlaper">
-        <h3>Welcome to Works Manager,Art works, and more</h3>
-        <span className="reg-email-wrapper">
-          <Input
-            id={addEmailInputId}
-            type="email"
-            placeholder="Register your email"
-            autoComplete="on"
-            autoFocus={true}
-            value={emailToRegister}
-            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setEmailToRegister(ev.target.value)}
-          />
-          <Button type="submit" variation={ButtonVariation.error}>
-            Register
-          </Button>
-        </span>
-      </div>
-    </Main>
+        <div className="home-container__overlaper">
+          <h2>Welcome to Works Manager,Art works, and more.</h2>
+          <p>Ready to upload your works? Enter your email to register.</p>
+          <span className="reg-email-wrapper" role="row" tabIndex={0}>
+            <Input
+              id={addEmailInputId}
+              type="email"
+              placeholder="Register your email"
+              autoComplete="on"
+              autoFocus={true}
+              value={emailToRegister}
+              onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setEmailToRegister(ev.target.value)}
+            />
+            <Button type="submit" variation={ButtonVariation.error}>
+              Register
+            </Button>
+          </span>
+        </div>
+      </Main>
+
+      <Footer />
+    </>
   );
 };
 
--- src/components/structures/footer/footer.component.tsx
+++ src/components/structures/footer/footer.component.tsx
@@ -1,1 +1,28 @@
-export {};
+import React from 'react';
+import './footer.style.scss';
+
+export const Footer: React.FC = (): JSX.Element => {
+  return (
+    <footer className="footer">
+      <div className="footer__row1" role="row">
+        <div>
+          <i className="fas fa-home-lg-alt"></i>
+          Home
+        </div>
+
+        <div>
+          <i className="fas fa-folder-open"></i>
+          View Works
+        </div>
+
+        <div>
+          <i className="fad fa-folder-plus"></i>
+          Add Work
+        </div>
+      </div>
+      <div className="footer__row2" role="row"></div>
+    </footer>
+  );
+};
+
+export default Footer;
