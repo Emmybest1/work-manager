0ef23f2ee1be213215576482704a3501165240c8
EMMYbest1 <emmajerry2016@gmail.com>
2021-03-07 21:03:51 +0200

    Feature: client api-request

--- src/client/https-request.ts
+++ src/client/https-request.ts
@@ -1,1 +1,15 @@
-export {};
+import axios, {AxiosPromise} from 'axios';
+
+export const $app__api = () => ({
+  get(url: string, headers?: any): AxiosPromise<any> {
+    return axios({method: 'get', url, headers});
+  },
+
+  post(url: string, data: any, headers?: any): AxiosPromise<any> {
+    return axios({method: 'post', url, data, headers});
+  },
+
+  delete(url: string, id: string, headers?: any): AxiosPromise<any> {
+    return axios({method: 'delete', url: `${url}${id}`, headers});
+  },
+});
