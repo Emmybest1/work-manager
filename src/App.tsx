89933f765c091a4f1697911e93adb43f9fb0a10a
EMMYbest1 <emmajerry2016@gmail.com>
2021-03-08 02:38:41 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './routes/routes.component';
 import Footer from './components/structures/footer/footer.component';
-
 import './sass/general.style.scss';
 
 const App: React.FC = (): JSX.Element => {
