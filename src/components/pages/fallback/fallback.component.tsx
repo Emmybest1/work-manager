16dd6ae4797d77c5e0099d9cbc249be18532f7ef
EMMYbest1 <emmajerry2016@gmail.com>
2021-03-09 20:48:51 +0200

    Feature

--- src/components/pages/fallback/fallback.component.tsx
+++ src/components/pages/fallback/fallback.component.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
+import Button from '../../partials/button/button.component';
+import Main from '../../structures/main/main.component';
 import './fallback.style.scss';
 
-const Fallback: React.FC = (): JSX.Element => {
-  return <div>Hello Fallback</div>;
+type TFallbackProps = {
+  message?: string;
+  fallbackPath?: string;
+};
+export const Fallback: React.FC<TFallbackProps> = ({message, fallbackPath}): JSX.Element => {
+  const history = useHistory();
+
+  return (
+    <Main>
+      <div className="fallback-container">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/error.svg`} alt="" />
+
+        <p>{message ? message : 'Sorry, the page you try to access is unavailable.'}</p>
+
+        <Button onClick={() => history.push(fallbackPath ?? '/')}>Go Home</Button>
+      </div>
+    </Main>
+  );
 };
 
 export default Fallback;
